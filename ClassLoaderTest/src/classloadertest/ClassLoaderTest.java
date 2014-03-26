5612255bfe6448563e3ab977e20b509120cedc90
volptar <volpe1988@gmail.com>
2014-03-26 15:59:39 -0400

    Remove Custom Class Loader

--- ClassLoaderTest/src/classloadertest/ClassLoaderTest.java
+++ ClassLoaderTest/src/classloadertest/ClassLoaderTest.java
@@ -16,7 +16,8 @@ public class ClassLoaderTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        CustomURLClassLoader loader = new CustomURLClassLoader();
+        
     }
     
 }
